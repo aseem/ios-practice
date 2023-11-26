7f2611e6eff2024be8e149778c64d37c47cf715f
Aseem Kohli <aseemko@gmail.com>
2023-11-26 10:03:10 -0500

    first commit

---
+++ AboutApp/AboutApp/AboutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutAppApp.swift
+//  AboutApp
+//
+//  Created by Aseem Kohli on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct AboutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
